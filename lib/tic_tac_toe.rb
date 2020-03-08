e7a4fc2845539b3cef9708a7f3cb24f1bcdb2e5e
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-03-08 22:01:09 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -106,8 +106,8 @@ def play(board)
   until over?(board) == true
     turn(board)
   end
-
     if won?(board)
+      winner(board) == "X" || winner(board) == "O"
       puts "Congratulations #{winner(board)}!"
     elsif draw?(board) == true
         puts "Cat's Game!"
